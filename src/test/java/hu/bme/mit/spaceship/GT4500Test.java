afbfaa243dd01b4ed90369618356c58fb7e15b2d
Horvath Patrik <horpatrik012@gmail.com>
2020-04-24 17:43:18 +0200

    Code coverage, more tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -138,4 +138,47 @@ public class GT4500Test {
     verify(mockedTorpedoStore2, times(0)).fire(1);
   }
 
+  @Test
+  public void fireTorpedo_Single_Two(){
+    // Arrange
+    when(mockedTorpedoStore1.fire(1)).thenReturn(true);
+    when(mockedTorpedoStore1.isEmpty()).thenReturn(false);
+    when(mockedTorpedoStore2.fire(1)).thenReturn(true);
+    when(mockedTorpedoStore2.isEmpty()).thenReturn(false);
+
+    // Act
+    boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result1);
+    assertEquals(true, result2);
+    verify(mockedTorpedoStore1, times(1)).isEmpty();
+    verify(mockedTorpedoStore1, times(1)).fire(1);
+    verify(mockedTorpedoStore2, times(1)).isEmpty();
+    verify(mockedTorpedoStore2, times(1)).fire(1);
+  }
+
+  @Test
+  public void fireTorpedo_Single_TwoOnlyFirstSuccess(){
+    // Arrange
+    when(mockedTorpedoStore1.fire(1)).thenReturn(true);
+    when(mockedTorpedoStore1.isEmpty()).thenReturn(false);
+    when(mockedTorpedoStore2.fire(1)).thenReturn(false);
+    when(mockedTorpedoStore2.isEmpty()).thenReturn(true);
+    boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    when(mockedTorpedoStore1.fire(1)).thenReturn(false);
+    when(mockedTorpedoStore1.isEmpty()).thenReturn(true);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result1);
+    assertEquals(false, result2);
+    verify(mockedTorpedoStore1, times(2)).isEmpty();
+    verify(mockedTorpedoStore1, times(1)).fire(1);
+    verify(mockedTorpedoStore2, times(1)).isEmpty();
+    verify(mockedTorpedoStore2, times(0)).fire(1);
+  }
+
 }
